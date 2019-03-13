22ad0639ae0eabbcbd00096a5257512b34beaa1d
吴为梁 <nhsoft.wwl@hotmail.com>
2019-03-13 21:51:22 +0900

    add rails

---
+++ app/controllers/fwp_controller.rb
@@ -0,0 +1,25 @@
+class FwpController < ApplicationController
+  def index
+    
+  end
+
+  def one
+    
+  end
+
+  def two
+    
+  end
+
+  def three
+    
+  end
+
+  def four
+    
+  end
+
+  def five
+    
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'fwp#index'
+
+  resources :fwp do
+    collection do 
+      get "one"
+      get "two"
+      get "three"
+      get "four"
+      get "five"
+    end
+  end
+end
